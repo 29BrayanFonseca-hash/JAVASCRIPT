49bdb2158f75a2acbd705e806696cb2a3eaff2a3
29BrayanFonseca-hash <249781083+29BrayanFonseca-hash@users.noreply.github.com>
2026-04-07 12:04:37 -0500

    first commit

---
+++ refuerzo/ejercicio 1/calculadora.js
@@ -0,0 +1,5 @@
+const previousOperand = document.getElementById("previous-operand");
+const currentOperand = document.getElementById("current-operand");
+const limpiar = document.getElementById("clear");
+const borrar = document.getElementById("delete");
+const igual = document.getElementById("equals");
